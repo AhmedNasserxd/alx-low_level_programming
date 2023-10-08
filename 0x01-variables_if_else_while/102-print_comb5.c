11c739b03614293198dbca77f7e9b00a550624df
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-09 00:49:06 +0300

    ok last time

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,24 @@ int main(void)
 int num1, num2;
 for (num1 = 0; num1 <= 99; num1++)
 {
-for (num2 = num1; num2 <=99; num2++)
+for (num2 = 0; num2 <= 99; num2++)
 {
-int tens1 = num1 / 10;
-int ones1 = num1 % 10;
-int tens2 = num2 / 10;
-int ones2 = num2 % 10;
-
-putchar(tens1 + '0');
-putchar(ones1 + '0');
+if (num1 < num2)
+{
+putchar(num1 / 10 + 48);
+putchar(num1 % 10 + 48);
 putchar(' ');
-putchar(tens2 + '0');
-putchar(ones2 + '0');
+putchar(num2 / 10 + 48);
+putchar(num2 % 10 + 48);
 
-if (num1 != 99 || num2 != 99)
+if (num1 != 98 || num2 != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-
+}
 putchar('\n');
 return (0);
 }
