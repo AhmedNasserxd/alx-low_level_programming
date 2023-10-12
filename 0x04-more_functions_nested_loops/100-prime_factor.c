cf778cd8f20491168c6d27dc44b09882b664d388
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-13 00:42:49 +0300

    New to this

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+*main - to get the largest prime of 612852475143
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+long number = 612852475143;
+long largest = -1;
+
+while (number % 2 == 0)
+{
+largest = 2;
+number /= 2;
+}
+
+for (long i = 3; i <= sqrt(number); i += 2)
+{
+while (number % i == 0)
+{
+largest = i;
+number /= i;
+}
+}
+
+if (number > 2)
+largest = number;
+
+printf("%ld\n", largest);
+
+return (0);
+}
