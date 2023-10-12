4e4ee934d0c80070deff0f9e73b6a1bfacd1df5f
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-12 17:06:51 +0300

    This is new thing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*main - to see the function which prints positive or negative
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0;
+positive_or_negative(i);
+
+return (0);
+}
