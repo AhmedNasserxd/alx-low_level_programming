e251d9342c8564a54ab8ca4acaa269dadaa47950
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-13 01:06:06 +0300

    let's do this

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,6 +10,7 @@ int main(void)
 {
 long number = 612852475143;
 long largest = -1;
+long i;
 
 while (number % 2 == 0)
 {
@@ -17,7 +18,7 @@ largest = 2;
 number /= 2;
 }
 
-for (long i = 3; i <= sqrt(number); i += 2)
+for (i = 3; i <= sqrt(number); i += 2)
 {
 while (number % i == 0)
 {
