f4144a33480f10081d7328d0108d2c694686774c
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-17 00:37:00 +0300

    please

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,30 +8,32 @@
 int _atoi(char *s)
 {
 int sign = 1;
-int result = 0;
+int num = 0;
 int i = 0;
 
-while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-s[i] == '\r' || s[i] == '\f' || s[i] == '\v')
-{
-i++;
-}
-
-if (s[i] == '-')
+if (s[0] == '-')
 {
 sign = -1;
 i++;
 }
-else if (s[i] == '+')
+
+while (s[i])
 {
+if (s[i] >= '0' && s[i] <= '9')
+{
+if (sign == 1 && (num > 214748364 || (num == 214748364 && s[i] - '0' > 7)))
+return (2147483647);
+if (sign == -1 && (num > 214748364 || (num == 214748364 && s[i] - '0' > 8)))
+return (-2147483648);
+
+num = num * 10 + (s[i] - '0');
+}
+else if (num != 0)
+{
+break;
+}
 i++;
 }
 
-while (s[i] >= '0' && s[i] <= '9')
-{
-result = result * 10 + (s[i] - '0');
-i++;
-}
-
-return result * sign;
+return (num * sign);
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,32 +3,24 @@
 #include <time.h>
 
 /**
-*main - generate random valid passwords
+*main - generate a valid password for
 *Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-char password[84];
-int sum = 0;
-int i, r;
+char password[15];
+int i;
+unsigned int seed;
 
-srand(time(NULL));
+seed = time(NULL);
+srand(seed);
 
-for (i = 0; sum < 2772 - 122; i++)
+for (i = 0; i < 14; i++)
 {
-r = rand() % 62;
-
-if (r < 10)
-password[i] = '0' + r;
-else if (r < 36)
-password[i] = 'a' + r - 10;
-else
-password[i] = 'A' + r - 36;
-
-sum += password[i];
+password[i] = rand() % 94 + 33;
 }
+password[14] = '\0';
 
-password[i] = 2772 - sum - '\0';
 printf("%s", password);
 
 return (0);
