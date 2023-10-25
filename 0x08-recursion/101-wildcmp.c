13a33cf042c9813939b49fb70415aeb2f692153d
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-25 20:11:29 +0300

    OKAY

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,16 +15,16 @@ return (*s1 == '\0');
 
 if (*s2 == '*') {
 if (wildcmp(s1, s2 + 1)) {
-return 1;
+return (1);
 }
 if (*s1 != '\0' && wildcmp(s1 + 1, s2)) {
-return 1;
+return (1);
 }
 }
 
 if (*s1 == *s2) {
-return wildcmp(s1 + 1, s2 + 1);
+return (wildcmp(s1 + 1, s2 + 1));
 }
 
-return 0;
+return (0);
 }
