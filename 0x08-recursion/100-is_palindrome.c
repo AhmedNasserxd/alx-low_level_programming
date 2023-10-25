6dea21e5e37c05442d41fb0812c98d0c98f16d72
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-25 20:10:49 +0300

    OK

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,42 @@
 #include "main.h"
 
+int _strlen(char *s);
 int check_palindrome(char *s, int start, int end);
 
 /**
-*is_palindrome - to check if a string is a palindrome
-*@s: string to check
+*_strlen - to calculate length of a string
+*@s: string to use
 *Return: the result
 */
 
+int _strlen(char *s)
+{
+if (*s == '\0')
+return (0);
+return (1 + _strlen(s + 1));
+}
+
+/**
+*is_palindrome - checks if a string is a palindrome
+*@s: string to check
+*Return: result
+*/
 int is_palindrome(char *s)
 {
-int length = 0;
-
-while (s[length] != '\0')
-length++;
+int length = _strlen(s);
 
 if (length <= 1)
 return (1);
 
-return check_palindrome(s, 0, length - 1);
+return (check_palindrome(s, 0, length - 1));
 }
 
 /**
-*check_palindrome - to check if a string is a palindrome
+*check_palindrome - checks if a string is a palindrome
 *@s: string to check
 *@start: starting index
 *@end: ending index
-*Return: the result
+*Return: result
 */
 int check_palindrome(char *s, int start, int end)
 {
@@ -34,7 +44,7 @@ if (start >= end)
 return (1);
 
 if (s[start] == s[end])
-return check_palindrome(s, start + 1, end - 1);
+return (check_palindrome(s, start + 1, end - 1));
 
 return (0);
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,23 +6,25 @@
 *@s2: the special string
 *Return: result
 */
+
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '\0' && *s1 == '\0')
-return (1);
+if (*s2 == '\0') {
+return (*s1 == '\0');
+}
 
-if (*s2 == '*')
-{
-if (*(s2 + 1) != '\0')
-{
-if (*s1 == '\0')
-return (0);
-return wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2);
+if (*s2 == '*') {
+if (wildcmp(s1, s2 + 1)) {
+return 1;
+}
+if (*s1 != '\0' && wildcmp(s1 + 1, s2)) {
+return 1;
 }
 }
 
-if (*s1 == *s2)
+if (*s1 == *s2) {
 return wildcmp(s1 + 1, s2 + 1);
+}
 
-return (0);
+return 0;
 }
