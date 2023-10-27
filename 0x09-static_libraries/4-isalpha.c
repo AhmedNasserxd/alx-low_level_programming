8257f6617b88b0a457af57b5166be7c7a62e2905
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-10-27 21:50:18 +0200

    add2

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_isalpha - to check for alphabetic character
+*@c: checking character
+*Return: 1 lower or uppercase, 0 not alphabet
+*/
+
+int _isalpha(int c)
+{
+char loweralpha;
+char upperalpha;
+int x = 0;
+
+for (loweralpha = 'a'; loweralpha <= 'z'; loweralpha++)
+{
+for (upperalpha = 'A'; upperalpha <= 'Z'; upperalpha++)
+{
+if (c == loweralpha || c == upperalpha)
+x = 1;
+}
+}
+return (x);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_abs - computes absolute
+*@n: integer input
+*Return: the absolute value
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+
+else
+return (-n);
+}
