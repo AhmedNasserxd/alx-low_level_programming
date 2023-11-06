46d3353aeb697ebc4a5a52b939dd8de931a034dd
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-06 22:01:12 +0200

    new era

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
 /**
 *new_dog - to create a new dog
@@ -13,31 +11,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-
-if (name == NULL || owner == NULL)
-return (NULL);
+int name_length, owner_length, k;
+char *copied_name, *copied_owner;
 
 new_dog = malloc(sizeof(dog_t));
-
 if (new_dog == NULL)
 return (NULL);
 
-new_dog->name = strdup(name);
-if (new_dog->name == NULL)
+for (name_length = 0; name[name_length] != '\0'; name_length++)
+;
+for (owner_length = 0; owner[owner_length] != '\0'; owner_length++)
+;
+
+copied_name = malloc(sizeof(char) * (name_length + 1));
+if (copied_name == NULL)
 {
 free(new_dog);
 return (NULL);
 }
 
-new_dog->owner = strdup(owner);
-if (new_dog->owner == NULL)
+copied_owner = malloc(sizeof(char) * (owner_length + 1));
+if (copied_owner == NULL)
 {
-free(new_dog->name);
+free(copied_name);
 free(new_dog);
 return (NULL);
 }
 
+for (k = 0; k <= name_length; k++)
+copied_name[k] = name[k];
+for (k = 0; k <= owner_length; k++)
+copied_owner[k] = owner[k];
+
+new_dog->name = copied_name;
 new_dog->age = age;
+new_dog->owner = copied_owner;
 
 return (new_dog);
 }
