ef35813dcd6d918842b866dcf54cf139f65d58e3
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-06 20:28:14 +0200

    New files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*dog_t - typedef for struct
+*struct dog - structure for dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
+
+typedef struct dog dog_t;
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
