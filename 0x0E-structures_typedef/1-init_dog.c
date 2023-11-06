175b5f32717f34d36bfc6a7929ecd6e38ba20573
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-06 21:12:26 +0200

    edit things

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
 *int_dog - to initialize dog struct
@@ -11,7 +10,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d != NULL)
+if (d)
 {
 d->name = name;
 d->age = age;
