c36070b15fc355ac24888c31328cf30f10a25b25
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-08 22:57:38 +0200

    edit4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,27 +12,26 @@ int main(int argc, char *argv[])
 int x, z;
 int (*k)(int, int);
 
-x = atoi(argv[1]);
-z = atoi(argv[3]);
-k = get_op_func(argv[2]);
-
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
 
-if (argv[2][1] != '\0')
+if (k == NULL || (argv[2][1] != '\0'))
 {
 printf("Error\n");
 exit(99);
 }
 
-if (k == NULL)
+if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[3][0] == '0')
 {
 printf("Error\n");
 exit(99);
 }
+
+x = atoi(argv[1]);
+z = atoi(argv[3]);
 
 printf("%d\n", k(x, z));
 
