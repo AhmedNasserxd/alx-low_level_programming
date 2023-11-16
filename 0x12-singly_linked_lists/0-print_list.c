fa8df6e81b2ff21caff822e440fcc077300fcac7
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-16 21:55:37 +0200

    new editasd

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
 
 size_t print_list(const list_t *h)
 {
-size_t len = 0;
+size_t i = 0;
+
 while (h != NULL)
 {
 if (h->str == NULL)
 printf("[0] (nil)\n");
-
 else
 printf("[%d] %s\n", h->len, h->str);
 
-len++;
+i++;
 h = h->next;
 }
-return (len);
+return (i);
 }
