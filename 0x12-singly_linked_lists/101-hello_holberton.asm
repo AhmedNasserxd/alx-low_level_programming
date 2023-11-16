f369cb69ab9b44d432cce9dd1e0fee45bfefc2d6
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2023-11-16 22:37:10 +0200

    new edit to this

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,22 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 0
+ extern printf
 
-section .text
+ section .text
     global main
 
-extern printf
-extern scanf
+ main:
+    push rbp
 
-main:
-    ; Set up the stack frame for printf
-    sub rsp, 8
-
-    ; Call printf with the format string and hello string
-    mov rdi, format
-    mov rsi, hello
-    mov rax, 0          ; printf syscall number
+    mov rdi,fmt
+    mov rsi,msg
+    mov rax,0
     call printf
 
-    ; Clean up the stack frame
-    add rsp, 8
+    pop rbp
 
-    ; Exit the program
-    mov rax, 60         ; exit syscall number
-    xor rdi, rdi        ; exit code 0
-    syscall
+    mov rax,0
+    ret
+
+ section .data
+    msg: db "Hello, Holberton", 0
+    fmt: db "%s", 10, 0
+    
